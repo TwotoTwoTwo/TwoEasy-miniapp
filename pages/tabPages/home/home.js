97e9e1ef5807ebc05d9f2270e2b16de9e4dcab5b
wsjiu <527172036@qq.com>
2020-10-29 22:57:33 +0800

    小程序初

---
+++ pages/tabPages/home/home.js
@@ -0,0 +1,10 @@
+Page({
+  onShow: function () {
+    if (typeof this.getTabBar === 'function' &&
+      this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 0
+      })
+    }
+  }
+})
